3484e0dc0840575f80585a19970b6a2534f4ec84
Andrew Craft <amcraftuk@gmail.com>
2017-03-15 23:49:18 +0000

    handle signals correctly for clean shutdown

--- run-services.sh
+++ run-services.sh
@@ -2,10 +2,20 @@
 
 shutdown() {
     ${TEAMCITY_DIST}/bin/teamcity-server.sh stop
+    exit 0
 }
 
 rm -f ${TEAMCITY_LOGS}/*.pid
 
 trap 'shutdown' SIGTERM SIGINT SIGHUP
 
-/bin/bash -c  "${TEAMCITY_DIST}/bin/teamcity-server.sh run"
+${TEAMCITY_DIST}/bin/teamcity-server.sh start
+
+while [ ! -f ${TEAMCITY_DIST}/logs/teamcity-server.log ];
+do
+   echo -n "."
+   sleep 1
+done
+
+tail -F ${TEAMCITY_DIST}/logs/teamcity-server.log &
+wait
