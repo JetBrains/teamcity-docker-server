ceed11a97f86e2a173d3423a5cb455c2a229d442
Dmitry Tretyakov <dtretyakov@gmail.com>
2018-01-19 10:47:52 +0100

    Update build scripts for 1709 containers
    Issue: TW-52060

--- build.ps1
+++ build.ps1
@@ -1,2 +1,19 @@
-docker build -t teamcity-server:latest-windowsservercore -f windowsservercore/Dockerfile .
-docker build -t teamcity-server:latest-nanoserver -f nanoserver/Dockerfile .
+# 1709 images
+docker build `
+ -t teamcity-server:latest-windowsservercore-1709 `
+ -f windowsservercore-1709/Dockerfile .
+
+docker build `
+ -t teamcity-server:latest-nanoserver-1709 `
+ -f nanoserver-1709/Dockerfile .
+
+# old images
+docker build --isolation=hyperv `
+ -t teamcity-server:latest-windowsservercore `
+ -t teamcity-server:latest-windowsservercore-ltsc2016 `
+ -f windowsservercore-ltsc2016/Dockerfile .
+
+docker build --isolation=hyperv `
+ -t teamcity-server:latest-nanoserver `
+ -t teamcity-server:latest-nanoserver-sac2016 `
+ -f nanoserver-sac2016/Dockerfile .
