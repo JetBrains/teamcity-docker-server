b47dea4ba7680c2e38d35cdd92c55e9b737ae559
Dmitry Tretyakov <dtretyakov@gmail.com>
2019-02-18 17:01:27 +0100

    Provide Windows 1809 containers
    Issue: TW-58161

--- build.ps1
+++ build.ps1
@@ -1,14 +1,8 @@
 # Build semi annual images
-$tags = @("1803")
+$tags = @("1803", "1809")
 foreach ($tag in $tags) {
  docker build `
  --build-arg TAG=$tag `
  -t "teamcity-server:latest-nanoserver-$tag" `
  -f nanoserver/Dockerfile .
 }
-
-# Build images with long term support
-docker build --isolation=hyperv `
- -t teamcity-server:latest-nanoserver `
- -t teamcity-server:latest-nanoserver-sac2016 `
- -f nanoserver-sac2016/Dockerfile .
