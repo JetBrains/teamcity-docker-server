9efbcf4f43b595bb01dfa084997f30dc5e2b88a2
Dmitry Tretyakov <dtretyakov@gmail.com>
2019-02-26 13:36:18 +0100

    Create separate Docker file for each windows image
    Due to problems in base images, e.g. powershell we my need to create
    a custom builds pipeline for each container version.
    
    Issue: TW-58161

--- build.ps1
+++ build.ps1
@@ -1,8 +1,7 @@
-# Build semi annual images
 $tags = @("1803", "1809")
+
 foreach ($tag in $tags) {
  docker build `
- --build-arg TAG=$tag `
  -t "teamcity-server:latest-nanoserver-$tag" `
- -f nanoserver/Dockerfile .
+ -f "nanoserver/$tag/Dockerfile" .
 }
